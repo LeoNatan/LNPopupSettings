badd9c6e58f15c4f52dd3d0a7304375748fa975d
Léo Natan <leo.natan@outlook.com>
2026-07-03 02:20:56 +0300

    Ignore UIDesignRequiresCompatibility on iOS 27

--- SettingKeys.m
+++ SettingKeys.m
@@ -232,7 +232,12 @@ BOOL LNPopupSettingsHasOS26Glass(void)
 	
 	static dispatch_once_t onceToken;
 	dispatch_once(&onceToken, ^{
-		if(@available(iOS 26.0, *))
+		if(@available(iOS 27.0, *))
+		{
+			//iOS 27 does not honor UIDesignRequiresCompatibility.
+			rv = YES;
+		}
+		else if(@available(iOS 26.0, *))
 		{
 			rv = ![[NSBundle.mainBundle objectForInfoDictionaryKey:@"UIDesignRequiresCompatibility"] boolValue];
 		}
